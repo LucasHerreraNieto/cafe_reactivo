2360ecec8239ce8d7ecbdab8822ea73e37dc3698
LucasHerreraNieto <lucassantiago.herreranieto@estudiantes.unahur.edu.ar>
2025-03-14 19:26:45 -0300

    DNI_footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,7 +1,8 @@
 const Footer = () => {
     return ( 
         <footer className="bg-dark text-light p-3 bottom ">
-             <p className="text-center">Desarrollado por: Lucas Santiago Herrera Nieto | Final Construcciones de interfaces de usuario.</p>        
+             <p className="text-center">Desarrollado por: Lucas Santiago Herrera Nieto | DNI:42474590</p> 
+             <p className="text-center">Final Construcciones de interfaces de usuario.</p>       
         </footer>
      );
 }
